f9995ddb905edcbc44823a145e713a2a990489fa
Marc <marcmde95@gmail.com>
2016-03-01 00:06:04 +0100

    Updated LICENSE & started with gameplay code

--- src/screens/screen_gameplay.c
+++ src/screens/screen_gameplay.c
@@ -1,8 +1,12 @@
 /**********************************************************************************************
+*   
+*   Tap To JAmp - gameplay_screen
+*
+*   Tap To JAmp is a The Impossible Game / Geometry Dash clone developed by Marc Montagut (@MarcMDE)
+*   
+-----------------------------------------------------------------------------------------------
 *
 *   raylib - Advance Game template
-*
-*   Gameplay Screen Functions Definitions (Init, Update, Draw, Unload)
 *
 *   Copyright (c) 2014 Ramon Santamaria (Ray San - raysan@raysanweb.com)
 *
@@ -25,6 +29,15 @@
 
 #include "raylib.h"
 #include "screens.h"
+#include "satcollisions.h"
+#include "c2dmath.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define GAME_SPEED 60
+
+#define CELL_SIZE 32
+#define ASSETS_SCALE 1
 
 //----------------------------------------------------------------------------------
 // Structs Definition (local to this module)
@@ -45,23 +58,61 @@ typedef struct Camera2D
     bool isMoving;
 } Camera2D;
 
-typedef struct StaticObject
+typedef struct Easing
 {
-    Vector2 position;
-    bool isActive;
-}
+    float t, b, c, d;
+    bool isFinished;
+}Easing;
 
-typedef struct RectCollider
+typedef struct DynamicObject
 {
-    Rectangle bounds;
-    bool isActive;
-}
+    //Rectangle checker;
+    Vector2 direction;
+    Vector2 speed;
+    Vector2 velocity;
+    bool isGrounded;
+    //int onAirCounter;
+}DynamicObject;
 
-typedef struct TriPointsCollidier
+typedef struct Player
 {
-    Vector2 pointsPosition[];
+    Transform2D transform;
+    DynamicObject dynamic;
+    SATBox boxCollider;
+    Easing rotationEasing;
+    bool isAlive;
+    Texture2D texture;
+    Color color;
+}Player;
+
+typedef struct ObjectStates
+{
     bool isActive;
-}
+    bool isDrawed;
+    bool isUp;
+}ObjectStates;
+
+typedef struct BoxGameObject
+{
+    Transform2D transform;
+    SATBox boxCollider;
+    ObjectStates state;
+}BoxGameObject;
+
+typedef struct TriGameObject
+{
+    Transform2D transform;
+    SATTri triCollider;
+    ObjectStates state;
+}TriGameObject;
+
+typedef struct GravityForce
+{
+    Vector2 direction;
+    float value;
+    Vector2 force;
+}GravityForce;
+//----------------------------------------------------------------------------------
 
 //----------------------------------------------------------------------------------
 // Global Variables Definition (local to this module)
@@ -71,16 +122,41 @@ typedef struct TriPointsCollidier
 static int framesCounter;
 static int finishScreen;
 
+Vector2 gridLenght;
+int groundY;
+GravityForce gravity;
+
+Player player;
+//----------------------------------------------------------------------------------
+
 //----------------------------------------------------------------------------------
 // Gameplay Screen Functions Definition
+//----------------------------------------------------------------------------------
+Vector2 GetOnGridPosition(Vector2 coordinates)
+void InitPlayer(Player *p, Vector2 coordinates, Vector2 speed, float rotationSpan);
+void SetPlayerAsGrounded(Player *p, int landPositionY)
 //----------------------------------------------------------------------------------
 
 // Gameplay Screen Initialization logic
 void InitGameplayScreen(void)
 {
-    // TODO: Initialize GAMEPLAY screen variables here!
     framesCounter = 0;
     finishScreen = 0;
+    
+    gridLenght.x = GetScreenWidth()/CELL_SIZE;
+    gridLenght.y = GetScreenHeight()/CELL_SIZE;
+    
+    InitPlayer(&player, (Vector2){6, gridLenght.y-3}, (Vector2){0, 5}, 0.75f);
+    
+    // Set ground position in the bottom of the desired cell. 
+    groundY = GetOnGridPosition((Vector2){0, gridLenght.y-3}).y + CELL_SIZE/2;
+    
+    // Set gravity
+    gravity.direction = (Vector2){0, 1};
+    gravity.value = 4;
+    gravity.force = Vector2FloatProduct(gravity.direction, value);
+    
+    
 }
 
 // Gameplay Screen Update logic
@@ -98,7 +174,6 @@ void UpdateGameplayScreen(void)
 // Gameplay Screen Draw logic
 void DrawGameplayScreen(void)
 {
-    // TODO: Draw GAMEPLAY screen here!
     DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), PURPLE);
     DrawText("GAMEPLAY SCREEN", 20, 20, 40, MAROON);
     DrawText("PRESS ENTER to JUMP to ENDING SCREEN", 170, 220, 20, MAROON);
@@ -107,11 +182,94 @@ void DrawGameplayScreen(void)
 // Gameplay Screen Unload logic
 void UnloadGameplayScreen(void)
 {
-    // TODO: Unload GAMEPLAY screen variables here!
+    
 }
 
 // Gameplay Screen should finish?
 int FinishGameplayScreen(void)
 {
     return finishScreen;
+}
+
+// Returns the position (cell center) based on the coordinates over the current grid
+Vector2 GetOnGridPosition(Vector2 coordinates)
+{
+    Vector2 position;
+    Vector2Scale(&coordinates, CELL_SIZE);
+    position.x = coordinates + CELL_SIZE/2;
+    position.y = coordinates + CELL_SIZE/2;
+    return coordinates; 
+}
+
+void InitPlayer(Player *p, Vector2 coordinates, Vector2 speed, float rotationSpan)
+{
+    // Init p transform
+    p->transform.position = GetOnGridPosition(coordinates);
+    p->transform.rotation = 0;
+    p->transform.scale = 1;
+    
+    // Init p dynamic
+    p->dynamic.direction = (Vector2){0, -1};
+    p->dynamic.speed = speed;
+    p->dynamic.velocity = Vector2Zero();
+    p->isGrounded = false;
+    
+    // TODO: Set player texture
+    //p->texture = LoadTexture("");
+    
+    // Set p color
+    p->color = WHITE;
+    
+    // Init p boxCollider
+    InitSATBox(&p->boxCollider, p->transform.position, (Vector2){p->texture.width, p->texture.height}, p->transform.rotation);
+    
+    // Init easing
+    p->rotationEasing.t = 0;
+    p->rotationEasing.b = 0;
+    p->rotationEasing.c = 180; // Try with 90
+    p->rotationEasing.d = rotationSpan;
+    p->rotationEasing.isFinished = false;
+}
+
+void UpdatePlayer(Player *p)
+{
+    
+    // Set player isGrounded to false since it has to be checked every frame (player would be falling from a platform)
+    p->isGrounded = false; 
+    
+    // Check if player landed on the ground
+    if (player->transform.position.y <= groundY) SetPlayerAsGrounded(&p, groundY)
+    
+    // TODO: Check if player landed (or collided) on a platform.
+    
+    // TODO: Check if player collided with a triangle
+
+    if (!p->isGrounded)
+    {
+        // Add gravity force
+        p->dynamic.velocity = Vector2Add(p->dynamic.velocity, gravity.force);
+    }
+    else
+    {
+        // Check jump key
+        if (IsKeyPressed(KEY_SPACE)) 
+        {
+            p->isGrounded = false;
+            
+            // Apply jump force
+            p->velocity = Vector2Product(p->dynamic.direction, p->dynamic.speed);
+        }
+        
+        // TODO: Init easing
+    }
+    
+    // TODO: Update player transform
+    
+    // TODO: Update player collider
+}
+
+void SetPlayerAsGrounded(Player *p, int landPositionY)
+{
+    p->dynamic.velocity.y = 0;
+    p->transform.position.y = landPositionY - p->boxCollider.size.y/2;
 }
